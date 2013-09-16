72e7f26bf040aa8c8bf94a301b603b0db88d4ff8
Chad Burt <chad@underbluewaters.net>
2013-09-15 17:53:19 -0700

    Made generate_thumbnails.py windows friendly

--- pyrifera/generate_thumbnails.py
+++ pyrifera/generate_thumbnails.py
@@ -7,15 +7,31 @@
 
 import os
 import sys
-rootdir = sys.argv[1]
-for file in os.listdir(rootdir):
+import subprocess
+from os.path import normpath
+
+for file in os.listdir("."):
     if not '.mp4.jpg' in file and not '.py' in file:
-        file = file.replace(' ', '\ ')
-        command = 'ffmpeg -i %s%s -an -vframes 1 -ss 80:00:00 -s 480x320 %s%s.%%03d.jpg' % (rootdir, file, rootdir, file,)
-        os.system(command)
-        src = '%s%s.001.jpg' % (rootdir, file)
-        dst = '%s%s.jpg' % (rootdir, file)
+        file = normpath(file)
+        print file
+        subprocess.call([
+            'ffmpeg', 
+            '-ss',
+            '00:02:30',
+            '-i',
+            file, 
+            '-an', 
+            '-vframes',
+            '1',
+            '-s',
+            '480x320',
+            ('%s.%%03d.jpg' % (file, ))
+        ], shell=True)
+        #command = 'ffmpeg -i %s%s -an -vframes 1 -ss 80:00:00 -s 480x320 %s%s.%%03d.jpg' % (rootdir, file,)
+        #os.system(command)
+        src = normpath('%s.001.jpg' % (file, ))
+        dst = normpath('%s.jpg' % (file, ))
         print src
         print dst
-        os.system('mv %s %s' % (src, dst))
+        os.rename(src, dst)
         print "renamed"
